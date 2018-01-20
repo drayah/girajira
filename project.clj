ec1ccee893c44f586899b40548607417592af1db
Giovanni Martina <gio@popado.co>
2018-01-20 13:26:00 -0200

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject girajira "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Girajira - github and jira integration for development process automation"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
