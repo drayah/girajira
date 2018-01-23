f0b1b7e4b7ac3f9fd9f93e9500a7e7ff01ad8412
Giovanni Martina <drayah@gmail.com>
2018-01-23 08:49:14 -0200

    Remove copyright from readme

--- README.md
+++ README.md
@@ -54,4 +54,4 @@ To start a web server for the application, run:
 
 ## License
 
-Copyright © 2017
+MIT
--- test/girajira/api/github/post_test.clj
+++ test/girajira/api/github/post_test.clj
@@ -25,7 +25,7 @@
                    "created_at" "some date"
                    "updated_at" "some other date"}})
 
-(def expected-transformed-payload
+(def expected-opened-payload
   {:action "opened"
    :pull_request {:url "some_url"
                   :id 1234
@@ -35,7 +35,7 @@
                   :created_at "some date"
                   :updated_at "some other date"}})
 
-(def expected-transformed-merged-payload
+(def expected-merged-payload
   {:action "closed"
    :pull_request {:url "some_url"
                   :id 1234
@@ -60,13 +60,13 @@
 
 (facts "when keywordizing the raw github payload"
   (fact "it returns the payload with keys transformed to keywords"
-    (keywordize-request-body opened-payload) => expected-transformed-payload
-    (keywordize-request-body merged-payload) => expected-transformed-merged-payload))
+    (keywordize-request-body opened-payload) => expected-opened-payload
+    (keywordize-request-body merged-payload) => expected-merged-payload))
 
 (facts "when getting pull request data from a transformed payload"
   (fact "it returns a hash containing pull request data"
-    (pull-request-data expected-transformed-payload) => expected-pull-request-data
-    (pull-request-data expected-transformed-merged-payload) => expected-merged-pull-request-data))
+    (pull-request-data expected-opened-payload) => expected-pull-request-data
+    (pull-request-data expected-merged-payload) => expected-merged-pull-request-data))
 
 (facts "when handling the post request given an opened pull request"
   (fact "it moves the jira issue and returns http status 204 no content"
