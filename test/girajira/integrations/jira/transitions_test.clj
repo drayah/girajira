b64308e77f74118ec16b442397dcea45481bdc4d
Giovanni Martina <drayah@gmail.com>
2018-03-13 18:47:49 -0300

    WIP

--- test/girajira/integrations/jira/transitions_test.clj
+++ test/girajira/integrations/jira/transitions_test.clj
@@ -3,8 +3,11 @@
             [girajira.integrations.jira.transitions :refer :all]
             [girajira.integrations.jira.request :as request]))
 
-(facts "when getting the jira api transitions url"
+(facts "when getting the jira api transitions url given an issue-id"
   (fact "it returns the correct api url"
     (transitions-url "fake-issue-id") => "http://jira.api/issue/fake-issue-id/transitions"
     (provided
       (request/jira-api-url) => "http://jira.api")))
+
+(facts "when getting the transitions given an issue-id"
+  (fact "it performs a jjira api request and returns json data"))
