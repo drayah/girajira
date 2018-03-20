d33d052a3bfca583c7944006a69d7b445b31ea83
Giovanni Martina <drayah@gmail.com>
2018-03-20 10:19:50 -0300

    Improve jira transitions code codecoverage

--- test/girajira/integrations/jira/transitions_test.clj
+++ test/girajira/integrations/jira/transitions_test.clj
@@ -31,3 +31,29 @@
     (let [api-response {"expand" "transitions"
                         "transitions" ..transitions-data..}]
       (issue-transitions api-response) => ..transitions-data..)))
+
+(facts "when extracting available (kanban) columns given transitions data"
+  (fact "it returns a sequence of column name and column id pairs"
+    (let [transitions [{"id" "1" "name" "Todo"}
+                       {"id" "2" "name" "Doing"}
+                       {"id" "3" "name" "Code Review"}]]
+      (extract-kanban-columns transitions) => [{"Todo" "1"} {"Doing" "2"} {"Code Review" "3"}])))
+
+(facts "when getting (kanban) columns for a specific issue-id"
+  (fact "it calls the correct functions in order to extract the data"
+    (kanban-columns-for-issue ..issue-id..) => ..extracted-column-data..
+    (provided
+      (get-transitions ..issue-id..) => ..json-response..
+      (issue-transitions ..json-response..) => ..transitions-data..
+      (extract-kanban-columns ..transitions-data..) => ..extracted-column-data..)))
+
+(facts "when checking a (kanban) column against a given name"
+  (let [column {"Todo" "14"}]
+    (facts "and the names match"
+      (fact "it returns true"
+        (kanban-column-with-name? "TODO" column) => truthy
+        (kanban-column-with-name? "todo" column) => truthy
+        (kanban-column-with-name? "ToDo" column) => truthy))
+    (facts "and the names don't match"
+      (fact "it returns false"
+        (kanban-column-with-name? "bla" column) => falsey))))
