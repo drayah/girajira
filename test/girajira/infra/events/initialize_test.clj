312f78f0726434f3274674989dc6137484526045
Giovanni Martina <gio@popado.co>
2018-05-15 22:48:18 -0300

    Improve subscribers initialization test

--- test/girajira/infra/events/initialize_test.clj
+++ test/girajira/infra/events/initialize_test.clj
@@ -2,10 +2,13 @@
   (:require [midje.sweet :refer :all]
             [girajira.infra.events.initialize :refer :all]
             [girajira.infra.events.subscribers.jira-transition :as jira]
-            [girajira.infra.events.subscribers.example-subscriber :as example]))
+            [girajira.infra.events.subscribers.example-subscriber :as example]
+            [girajira.infra.events.pubsub :as pubsub]
+            [girajira.infra.events.definitions :as events]))
 
 (facts "when initializing subscribers"
   (fact "it initializes all event subscribers"
-    (initialize-subscribers) => :initialize-example-subscriber
+    (initialize-subscribers) => ..initialized-all-subscribers..
     (provided
-      (jira/initialize-subscriber) => :initialize-jira-subscriber)))
+      (pubsub/subscribe events/example-event anything example/subscriber) => ..initialized-example-subscriber..
+      (jira/initialize-subscriber) => ..initialized-all-subscribers..)))
